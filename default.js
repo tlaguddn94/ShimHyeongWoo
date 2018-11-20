2c8605e2dd6c71e253a52a1f16083114b3c8ba53
tlaguddn94 <45193475+tlaguddn94@users.noreply.github.com>
2018-11-20 22:17:35 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,3 +1,3 @@
-document.getElementById('container').innerHTML = Date()
+document.getElementById('container').innerHTML = Date();
 var cars = ["Saab", "Volvo", "BMW"];
 document.getElementById("demo").innerHTML = cars;
